bed8a654f120654b152d5ebf391d3d55b3db9bd2
oleg425top <oleg59152@gmail.com>
2025-10-23 16:40:15 +0500

    1. Изменил урлы, иначе была ошибка

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from account.views import user_login, dashboard
 
 urlpatterns = [
     # path('login/', user_login, name='login'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('login/', LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='account/registration/logout.html'), name='logout'),
     path('', dashboard, name='dashboard'),
 ]
