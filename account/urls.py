006374419bb6b4c5b05ed0432cf6c626396e1bf0
oleg425top <oleg59152@gmail.com>
2025-10-24 15:36:25 +0500

    1. Добавил урлы для смены пароля и успешной смены пароля

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     # path('login/', user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='account/registration/logout.html'), name='logout'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='account/registration/password_change_form.html'), name='password-change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='account/registration/password_change_done.html'), name='password_change_done'),
     path('', dashboard, name='dashboard'),
 ]
